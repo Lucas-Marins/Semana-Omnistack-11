45cccf0abd2315efd1dda5333dc67700cc41b78a
Lucas Marins <lmarins02@hotmail.com.br>
2020-10-01 16:13:52 -0300

    Finished Logon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import Header from './Header'
+import './global.css'
+
+import Logon from './pages/Logon'
 
 function App() {
   return (
-    <Header title="Semana omniStack"/>
+    <Logon />
   );
 }
 
