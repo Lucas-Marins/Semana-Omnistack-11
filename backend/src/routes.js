f377a03ea9effd6dc57f8c9d8b8d4812bc62ec3e
Lucas Marins <lmarins02@hotmail.com.br>
2020-09-30 23:16:37 -0300

    Finished backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,22 @@
 const express = require('express')
 
+const OngController = require('./controller/OngController')
+const IncidentController = require('./controller/IncedentController')
+const ProfileController = require('./controller/ProfileController')
+const SessionsControler = require('./controller/SessionController')
+
 const routes = express.Router();
 
+routes.post('/sessions', SessionsControler.create)
+
+routes.get('/ongs', OngController.index )
+routes.post('/ongs', OngController.create)
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete);
 
 
 
