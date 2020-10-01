4903532663b5fe7749addb452f4e24ce5a165e93
Lucas Marins <lmarins02@hotmail.com.br>
2020-09-30 21:08:17 -0300

    Configuring the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express();
+const routes = require('./routes')
 
-app.get('/', (req, res)=>{
 
-})
-
+app.use(express.json())
+app.use(routes);
 
 app.listen(3333)
